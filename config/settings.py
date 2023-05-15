2e65a956478fecb89d25d7a99f4763eba855462f
Zohidoff <zohidov.begzod@mail.ru>
2023-05-15 17:58:32 +0300

    ignore migration folder

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,13 @@
+import os
 from decouple import config
-
+from sqlalchemy.dialects.postgresql import psycopg2
 from starlette.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory='templates')
 
 
 class Settings:
-
-    PROJECT_NAME: str = "Course"
+    PROJECT_NAME: str = "Todo"
     PROJECT_VERSION: str = "1.0.0"
 
     USE_POSTGRESQL_DB: str = config("USE_POSTGRESQL_DB")
@@ -17,9 +17,9 @@ class Settings:
     POSTGRES_DB: str = config("POSTGRES_DB")
     POSTGRES_PORT: str = config("POSTGRES_PORT")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
     SECRET_KEY: str = config("SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 settings = Settings()
