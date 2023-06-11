72a5ff7194f2e1ded2a625e40ea1fe656ece51a2
b.zohidov <@zohidovbegzod4557@gmail.com>
2023-06-11 10:24:53 +0300

    password hasher function

---
+++ apps/accounts/hasher.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+
+
+def hash_password(password: str):
+    
+    hashed_password = pwd_context.hash(password)
+    
+    return hashed_password
+
+
+def verify(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
